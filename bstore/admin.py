5d14eaf6ccff11921ff2ed32b634ce2e8b766945
akhterrehan983 <56243095+akhterrehan983@users.noreply.github.com>
2020-07-09 19:54:17 +0530

    Delete admin.py

--- bstore/admin.py
+++
@@ -1,8 +0,0 @@
-from django.contrib import admin
-from bstore.models import U_table
-# Register your models here.
-
-class U_tableAdmin(admin.ModelAdmin):
-    list_display=['isbno','price']
-admin.site.register(U_table,U_tableAdmin)
-# admin.site.register(U_table)
